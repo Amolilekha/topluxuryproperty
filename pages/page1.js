29014218996fcb195f30af02a1d86b6979e97c35
Amolilekha <163150407+Amolilekha@users.noreply.github.com>
2024-03-15 17:12:18 +0530

    Create page1.js

---
+++ pages/page1.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Dubai Real Estate Karket Forecast 2024</title>
+       <meta name="google-site-verification" content="c0HXgOxwQqyryYBeb3PSFShMYlUtm152EvhvsgKLWsk" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="dubai real estate market forecast 2024" />
+        <p className="description">
+          <h2><a href="https://topluxuryproperty.com/">Top Luxury Property</a></h2> <code>pages/index.js</code>
+        </p>
+     <p className="description">
+          <h2>Top Luxury Property</h2> <code>pages/index.js</code>
+        </p>
+     <p className="description">
+          <h2>Top Luxury Property</h2> <code>pages/index.js</code>
+        </p>
+
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
