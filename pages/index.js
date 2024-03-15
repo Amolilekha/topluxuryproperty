4f6a8543066f10c9d514cf110a512a47bfd8f984
Amolilekha <163150407+Amolilekha@users.noreply.github.com>
2024-03-15 16:35:26 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Top Luxury Property - Dubai 2024</title>
+        <title>Dubai Real Estate Karket Forecast 2024</title>
+       <meta name="google-site-verification" content="c0HXgOxwQqyryYBeb3PSFShMYlUtm152EvhvsgKLWsk" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my App!" />
+        <Header title="dubai real estate market forecast 2024" />
         <p className="description">
-          <h2>Top Luxury Property</h2> <code>pages/index.js</code>
+          <h2><a href="https://topluxuryproperty.com/">Top Luxury Property</a></h2> <code>pages/index.js</code>
         </p>
      <p className="description">
           <h2>Top Luxury Property</h2> <code>pages/index.js</code>
